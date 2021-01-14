5204b4b8971e2d60d6fb63a9ed03040a3305eb4c
Vlad Setchin <vlad.setchin@gmail.com>
2021-01-14 13:48:23 +1100

    Completed blog routing setup and added a couple of empty post files

---
+++ blog/why-i-moved-from-vuepress-to-angular-with-scully.md
@@ -0,0 +1,9 @@
+---
+title: "Why I moved from Vuepress to Angular with Scully"
+description: "blog description"
+published: true
+slugs:
+  - moving-from-vuepress-to-angular
+---
+
+# Why I moved from Vuepress to Angular with Scully
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { IdleMonitorService, ScullyRoutesService } from '@scullyio/ng-lib';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(
+    private idle: IdleMonitorService,
+    public scully: ScullyRoutesService
+  ) {}
+
   title = 'ng-blog';
 }
