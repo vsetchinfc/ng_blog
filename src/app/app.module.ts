1e7b41d6dabeb6cee2525ce9f3c958de586e0bd4
Vlad Setchin <vlad.setchin@gmail.com>
2021-01-28 22:07:55 +1100

    Added component for blog footer, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BlogHeaderComponent } from './components/blog-header/blog-header.component';
+import { BlogFooterComponent } from './components/blog-footer/blog-footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BlogHeaderComponent, BlogFooterComponent],
   imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/blog-footer/blog-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blog-footer',
+  templateUrl: './blog-footer.component.html',
+  styleUrls: ['./blog-footer.component.scss'],
+})
+export class BlogFooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
