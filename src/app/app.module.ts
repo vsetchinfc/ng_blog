7989b4f720e63604b1223a3c5a930eab4f5ddd16
Vlad Setchin <vlad.setchin@gmail.com>
2021-02-01 16:06:03 +1100

    Added angular-feather package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { IconsModule } from './icons/icons.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { BlogHeaderComponent } from './components/blog-header/blog-header.component';
@@ -10,7 +11,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, BlogHeaderComponent, BlogFooterComponent],
-  imports: [BrowserModule, AppRoutingModule, ScullyLibModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ScullyLibModule,
+    NgbModule,
+    IconsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
