ca8035e2b64fccad95f86d7cbd51a4b1cb16b84b
Vlad Setchin <vlad.setchin@gmail.com>
2021-02-18 09:27:22 +1100

    Added config.json and config sevice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { IconsModule } from './icons/icons.module';
@@ -11,17 +11,43 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BlogPostCardComponent } from './components/blog-post-card/blog-post-card.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { IdCardComponent } from './components/id-card/id-card.component';
+import { BlogConfigService } from './services';
+import { HttpClientModule } from '@angular/common/http';
+
+const BLOG_CONFIG_PROVIDER = [
+  {
+    provide: APP_INITIALIZER,
+    multi: true,
+    deps: [BlogConfigService],
+    useFactory: (blogConfigService: BlogConfigService) => {
+      return () => {
+        debugger;
+        return blogConfigService.loadBlogConfig();
+      };
+    },
+  },
+];
+
+const PROVIDERS = [...BLOG_CONFIG_PROVIDER];
 
 @NgModule({
-  declarations: [AppComponent, BlogHeaderComponent, BlogFooterComponent, BlogPostCardComponent, SidebarComponent, IdCardComponent],
+  declarations: [
+    AppComponent,
+    BlogHeaderComponent,
+    BlogFooterComponent,
+    BlogPostCardComponent,
+    SidebarComponent,
+    IdCardComponent,
+  ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     ScullyLibModule,
     NgbModule,
     IconsModule,
   ],
-  providers: [],
+  providers: [...PROVIDERS],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class BlogAppModule {}
